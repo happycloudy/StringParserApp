2fde835fbd6dc9ac66a8c8e5387b3f5a760f5f7a
happycloudy <arion.mikolae71@mail.ru>
2021-03-01 16:35:04 +0300

    added  unlincsync()

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const lineReader = require ('line-by-line-reader');
 
 async function Init() {
     stopWatch = new StopWatch();
-    let filepath = "./input.txt";
+    let filepath = "../FileGenerator/test.txt";
     let input
     await lineReader(filepath)
         .then(fileContent => {
@@ -28,66 +28,25 @@ async function Init() {
 
 
     let path = './outputSortByLength.txt'
-    if (fs.existsSync(path)) fs.rmSync(path)
+    if (fs.existsSync(path)) fs.unlinkSync(path)
     ArrSortByLength.forEach((str) => {
         fs.appendFileSync('./outputSortByLength.txt', str + '\n', err => err) // запись в файл 
     })
 
     path = './outputSortByAlf.txt'
-    if (fs.existsSync(path)) fs.rmSync(path)
+    if (fs.existsSync(path)) fs.unlinkSync(path)
     ArrSortByAlf.forEach((str) => {
         fs.appendFileSync('./outputSortByAlf.txt', str + '\n', err => err) // запись в файл 
     })
 
     path = './outputCommonElements.txt'
-    if (fs.existsSync(path)) fs.rmSync(path)
+    if (fs.existsSync(path)) fs.unlinkSync(path)
     ArrOfCommonElements.forEach((str, ind) => {
         fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл 
     })
 
     console.log("Время выполнения " + stopWatch.duration()*1000 + 'мс' );
 
-
-
-
-    // fs.readFile('./input.txt',async (err,data) => {
-    //     if(err) return console.log("Нету файла input.txt");
-
-    //     let input = data.toString().split('\n')
-    //     if(input.join('') == '') return console.log("Файл пустой");
-        
-
-
-        // await stopWatch.start();
-        // let ArrSortByLength = [...input.sort((a, b) => a.length < b.length? -1 : 1)] // сортировка по длине строк
-
-        // let ArrSortByAlf = input.sort((a, b) => a.localeCompare(b)) // сорировка по алфавиту
-
-        // let ArrOfCommonElements = await FindCommEles(input, ArrSortByAlf[0]) // нахождение всех уникальных общих вхождений 
-        // await stopWatch.stop();
-
-
-
-        // let path = './outputSortByLength.txt'
-        // if(fs.existsSync(path)) fs.rmSync(path)
-        // ArrSortByLength.forEach((str)=>{
-        //     fs.appendFileSync('./outputSortByLength.txt', str+'\n', err => err) // запись в файл 
-        // })
-
-        // path = './outputSortByAlf.txt'
-        // if(fs.existsSync(path)) fs.rmSync(path)
-        // ArrSortByAlf.forEach((str)=>{
-        //     fs.appendFileSync('./outputSortByAlf.txt', str+'\n', err=>err) // запись в файл 
-        // })
-
-        // path = './outputCommonElements.txt'
-        // if(fs.existsSync(path)) fs.rmSync(path)
-        // ArrOfCommonElements.forEach((str,ind)=>{
-        //     fs.appendFile('./outputCommonElements.txt', str+'\n', err=>err) // запись в файл 
-        // })
-    
-        // console.log("Время выполнения " + stopWatch.duration()*1000 + 'мс' );
-    // })
     
 }
 Init() 
