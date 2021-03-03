b5432552f71b1e3dd881e652b974bcf83b768ecd
happycloudy <arion.mikolae71@mail.ru>
2021-03-03 14:58:10 +0300

    time deploying

--- index.js
+++ index.js
@@ -19,11 +19,19 @@ async function Init() {
         
     await stopWatch.start();
     let ArrSortByLength = [...input.sort((a, b) => a.length < b.length ? -1 : 1)] // сортировка по длине строк
-
+    await stopWatch.stop();
+    console.log("Время сортировки по длине" + stopWatch.duration()*1000 + 'мс\n' );
+    
+    
+    await stopWatch.start();
     let ArrSortByAlf = input.sort((a, b) => a.localeCompare(b)) // сорировка по алфавиту
-
+    await stopWatch.stop();
+    console.log("Время сортировки по алфавиту" + stopWatch.duration()*1000 + 'мс\n' );
+    
+    await stopWatch.start();
     let ArrOfCommonElements = await FindCommEles(input, ArrSortByAlf[0]) // нахождение всех уникальных общих вхождений 
     await stopWatch.stop();
+    console.log("Время нахождения вхождений" + stopWatch.duration()*1000 + 'мс\n' );
 
 
 
@@ -45,7 +53,6 @@ async function Init() {
         fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл 
     })
 
-    console.log("Время выполнения " + stopWatch.duration()*1000 + 'мс' );
 
     
 }
