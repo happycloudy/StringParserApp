6a60acb0550e389ef6fb95a5276f0a89a3ecb24a
happycloudy <arion.mikolae71@mail.ru>
2021-03-03 15:53:38 +0300

    quick sort

--- index.js
+++ index.js
@@ -19,17 +19,15 @@ async function Init() {
 
         
     await stopWatch.start();
-    // input = ['фа','вфыв','фмвыаыфыаффа','фывп','фвпфваф','бикпрука','ауйца']
-    // let ArrSortByLength = [...input.sort((a, b) => a.length < b.length ? -1 : 1)] // сортировка по длине строк
-    
-    let ArrSortByLength =[...array_timsort.sort(input,(a, b) => a.length < b.length ? -1 : 1)]
+    let ArrSortByLength = [...input.sort((a, b) => a.length < b.length ? -1 : 1)] // сортировка по длине строк
+    // let ArrSortByLength = [...quickSort(input)]
     await stopWatch.stop();
     console.log("Время сортировки по длине" + stopWatch.duration()*1000 + 'мс\n' );
     
     
     await stopWatch.start();
     // let ArrSortByAlf = input.sort((a, b) => a.localeCompare(b)) // сорировка по алфавиту
-    let ArrSortByAlf = [...array_timsort.sort(input,(a, b) => a.localeCompare(b))]
+    let ArrSortByAlf = [...quickSort(input)]
     await stopWatch.stop();
     console.log("Время сортировки по алфавиту" + stopWatch.duration()*1000 + 'мс\n' );
     
@@ -46,19 +44,60 @@ async function Init() {
         fs.appendFileSync('./outputSortByLength.txt', str + '\n', err => err) // запись в файл 
     })
 
-    // path = './outputSortByAlf.txt'
-    // if (fs.existsSync(path)) fs.unlinkSync(path)
-    // ArrSortByAlf.forEach((str) => {
-    //     fs.appendFileSync('./outputSortByAlf.txt', str + '\n', err => err) // запись в файл 
-    // })
+    path = './outputSortByAlf.txt'
+    if (fs.existsSync(path)) fs.unlinkSync(path)
+    ArrSortByAlf.forEach((str) => {
+        fs.appendFileSync('./outputSortByAlf.txt', str + '\n', err => err) // запись в файл 
+    })
 
-    // path = './outputCommonElements.txt'
-    // if (fs.existsSync(path)) fs.unlinkSync(path)
-    // ArrOfCommonElements.forEach((str, ind) => {
-    //     fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл 
-    // })
-
-
+    path = './outputCommonElements.txt'
+    if (fs.existsSync(path)) fs.unlinkSync(path)
+    ArrOfCommonElements.forEach((str, ind) => {
+        fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл 
+    })
     
 }
 Init() 
+
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? items.length - 1 : right;
+        index = partition(items, left, right);
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function swap(items, firstIndex, secondIndex){
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
