df1fd5ef9dfdd1bc6b25a2f280aa070f2c6a291b
happycloudy <arion.mikolae71@mail.ru>
2021-03-11 13:44:33 +0300

    release ver

--- index.js
+++ index.js
@@ -1,103 +1,61 @@
 const fs = require('fs')
 const FindCommEles = require('./modules/FindCommEles')
 const StopWatch = require('stopwatch-js')
-const lineReader = require ('line-by-line-reader');
-const array_timsort = require('array-timsort')
+const lineReader = require ('line-by-line')
+const quickSort = require('./modules/FastSort')
 
 async function Init() {
     stopWatch = new StopWatch();
-    let filepath = "../FileGenerator/input1.txt";
-    let input
-    await lineReader(filepath)
-        .then(fileContent => {
-            input = fileContent.lines
-            input = input.filter((str)=> str != 'init')
+    let filepath = "../FileGenerator/test.txt";
+    let input = []
+    lr = new lineReader(filepath);
+    lr.on('error', function (err) {
+        console.log(err);
+    });
+    lr.on('line', function (line) {
+        lr.pause();
+        input.push(line)
+        lr.resume()
+    });
+    lr.on('end', async function () {
+        await stopWatch.start();
+        let ArrSortByLength = [...input.sort((a, b) => a.length < b.length ? -1 : 1)] // сортировка по длине строк
+        await stopWatch.stop();
+        console.log("Время сортировки по длине " + stopWatch.duration() * 1000 + 'мс\n');
+
+
+        await stopWatch.start();
+        let ArrSortByAlf = [...quickSort(input)] // сорировка по алфавиту
+        await stopWatch.stop();
+        console.log("Время сортировки по алфавиту" + stopWatch.duration() * 1000 + 'мс\n');
+
+        await stopWatch.start();
+        let ArrOfCommonElements = await FindCommEles(input, ArrSortByAlf[0]) // нахождение всех уникальных общих вхождений  
+        await stopWatch.stop();
+        console.log("Время нахождения вхождений" + stopWatch.duration() * 1000 + 'мс\n');
+
+
+
+        let path = './outputSortByLength.txt'
+        if (fs.existsSync(path)) fs.unlinkSync(path)
+        ArrSortByLength.forEach((str) => {
+            fs.appendFileSync('./outputSortByLength.txt', str + '\n', err => err) // запись в файл по длине
         })
-        .catch(err => {
-            console.log(err);
-        });
 
-        
-    await stopWatch.start();
-    let ArrSortByLength = [...input.sort((a, b) => a.length < b.length ? -1 : 1)] // сортировка по длине строк
-    // let ArrSortByLength = [...quickSort(input)]
-    await stopWatch.stop();
-    console.log("Время сортировки по длине" + stopWatch.duration()*1000 + 'мс\n' );
-    
-    
-    await stopWatch.start();
-    // let ArrSortByAlf = input.sort((a, b) => a.localeCompare(b)) // сорировка по алфавиту
-    let ArrSortByAlf = [...quickSort(input)]
-    await stopWatch.stop();
-    console.log("Время сортировки по алфавиту" + stopWatch.duration()*1000 + 'мс\n' );
-    
-    await stopWatch.start();
-    let ArrOfCommonElements = await FindCommEles(input, ArrSortByAlf[0]) // нахождение всех уникальных общих вхождений 
-    await stopWatch.stop();
-    console.log("Время нахождения вхождений" + stopWatch.duration()*1000 + 'мс\n' );
+        path = './outputSortByAlf.txt'
+        if (fs.existsSync(path)) fs.unlinkSync(path)
+        ArrSortByAlf.forEach((str) => {
+            fs.appendFileSync('./outputSortByAlf.txt', str + '\n', err => err) // запись в файл по алфавиту
+        })
 
+        path = './outputCommonElements.txt'
+        if (fs.existsSync(path)) fs.unlinkSync(path)
+        ArrOfCommonElements.forEach((str, ind) => {
+            fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл общих вхождений
+        })
 
-
-    let path = './outputSortByLength.txt'
-    if (fs.existsSync(path)) fs.unlinkSync(path)
-    ArrSortByLength.forEach((str) => {
-        fs.appendFileSync('./outputSortByLength.txt', str + '\n', err => err) // запись в файл 
-    })
-
-    path = './outputSortByAlf.txt'
-    if (fs.existsSync(path)) fs.unlinkSync(path)
-    ArrSortByAlf.forEach((str) => {
-        fs.appendFileSync('./outputSortByAlf.txt', str + '\n', err => err) // запись в файл 
-    })
-
-    path = './outputCommonElements.txt'
-    if (fs.existsSync(path)) fs.unlinkSync(path)
-    ArrOfCommonElements.forEach((str, ind) => {
-        fs.appendFile('./outputCommonElements.txt', str + '\n', err => err) // запись в файл 
-    })
-    
+    });
 }
-Init() 
+Init()
 
 
-function quickSort(items, left, right) {
-    var index;
-    if (items.length > 1) {
-        left = typeof left != "number" ? 0 : left;
-        right = typeof right != "number" ? items.length - 1 : right;
-        index = partition(items, left, right);
-        if (left < index - 1) {
-            quickSort(items, left, index - 1);
-        }
-        if (index < right) {
-            quickSort(items, index, right);
-        }
-    }
-    return items;
-}
-
-function partition(items, left, right) {
-    var pivot   = items[Math.floor((right + left) / 2)],
-        i       = left,
-        j       = right;
-    while (i <= j) {
-        while (items[i] < pivot) {
-            i++;
-        }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(items, i, j);
-            i++;
-            j--;
-        }
-    }
-    return i;
-}
-
-function swap(items, firstIndex, secondIndex){
-    const temp = items[firstIndex];
-    items[firstIndex] = items[secondIndex];
-    items[secondIndex] = temp;
-}
